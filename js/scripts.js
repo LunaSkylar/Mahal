43ae24d01222b69c40ccaf3b447859eb05c1aa67
LunaSkylar <stephanie.saguns@gmail.com>
2024-12-03 23:09:33 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+/*!
+* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+function yes1(){
+    alert("Yey!");
+}
+
+function no1(){
+    alert("No, pili ka ulit! hmp!")
+}
+
+function yes2(){
+    alert("awwww");
+}
+
+function no2(){
+    alert("edi don't hmp! /*nagcry")
+}
+
+function yes3(){
+    alert("I admire you too!");
+}
+
+function no3(){
+    alert("so di mo ko love? :<<<<")
+}
+
+function yes4(){
+    alert("Yes!!!!!!!!!!! my wifeyyy mwapss");
+}
+
+function no4(){
+    alert("/*umiyak ng umiyak")
+}
+
+function yes5(){
+    alert("sure na sure na ha");
+}
+
+function no5(){
+    alert("tyt po mwaps")
+}
